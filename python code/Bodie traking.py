27d05af6bd47ad755fec22a67e2c5f6581f2a63b
BraxtonBrown <10944268@uvu.edu>
2025-03-26 10:14:40 -0600

    python code

---
+++ python code/Bodie traking.py
@@ -0,0 +1,21 @@
+﻿import cv2 # OpenCV
+from cvzone.PoseModule import PoseDetector # Pose detection module
+
+# Web Camera
+cap = cv2.VideoCapture(0) # Camera Number
+cap.set(3, 1280) # Width
+cap.set(4, 720) # Height
+
+# Pose Detector
+detector = PoseDetector()
+
+while True:
+    success, img = cap.read() # Detecting the camera and continue to read the camera if a camera is present
+
+    # Pose
+    img = detector.findPose(img)
+    lmList, bboxInfo = detector.findPosition(img, bboxWithHands=True)
+
+    # Display
+    cv2.imshow("Webcam", img)
+    cv2.waitKey(1)
---
+++ python code/Web Camerea Traking base.py
@@ -0,0 +1,25 @@
+﻿import cv2 # OpenCV
+from cvzone.HandTrackingModule import HandDetector # Hand tracking module
+
+# open cv and media pipe are part of the cvzone library you will need to install it and media pipe to run this code
+
+# Web Camera
+cap = cv2.VideoCapture(0) # Camera Number
+cap.set(3, 1280) # Width 
+cap.set(4, 720) # Height
+
+# Hand Detector
+detector = HandDetector(maxHands =4, detectionCon= 0.8)
+
+while True:
+    success, img = cap.read() # detecting the camera and continue to read the camera if a camera is present
+    
+    # Hands
+    hands, img = detector.findHands(img)
+    
+    # Transferring landmark data to Unity
+    
+    
+    # Display
+    cv2.imshow("Webcam", img)
+    cv2.waitKey(1)
